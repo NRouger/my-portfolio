b5a49a88cf634efeb9db19ee16738d4e09354fb3
nicolas rouger <nicorouger@outlook.com>
2023-07-25 23:39:58 +0200

    views responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,16 +15,18 @@ function Home() {
   return (
 
     <div className="container">
+      <div className="margin-home"></div>
+
       <div className="home-grid">
         <div data-aos='zoom-in'>
         <div>
-          <p className='subtitle'>Hi. my name is Nicolás Rouger, but call me</p>
+          <p className='subtitle-mobile'>Hi. my name is Nicolás Rouger, but call me</p>
           <div>
             <TypingEffect text="Nico" className="title" />
           </div>
-          <p className="" style={{margin: "1rem"}}>I'm a FullStack Web Developer.
+          <p className="text-mobile" style={{margin: "1rem"}}>I'm a FullStack Web Developer.
           Passionate about the development of a user experience from scratch.</p>
-          <div className='line-left'></div>
+          <div className='line-left' style={{margin: "3em 1em"}}></div>
           <div>
             <Link to="/portfolio"> {/*data-aos="fade" data-aos-offset="100"*/}
               <button className="material-bubble" style={{ margin: "3rem 1rem"}}>Get to know me</button>
